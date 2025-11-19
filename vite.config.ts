e33447886ee2e08ba51b99daabb72ac2e3b7a7ea
detzahard <152923605+detzahard@users.noreply.github.com>
2025-11-18 22:46:49 -0300

    Fixed bulma

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      sass: {
+        silenceDeprecations: ["import", "global-builtin", "color-functions"],
+      },
+    },
+  },
 });
